3cc5b160270e0d10cf728bcb0004e1112a1ce751
donnieyoung <36338880@qq.com>
2026-02-22 17:22:27 +0800

    初步解决stock_analysis环境问题，可以运行，但是由于部分国外网址不能访问导致一些问题。

--- crews/stock_analysis/src/stock_analysis/crew.py
+++ crews/stock_analysis/src/stock_analysis/crew.py
@@ -1,45 +1,25 @@
-from typing import List
-from crewai import Agent, Crew, Process, Task
+from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task
 
 from tools.calculator_tool import CalculatorTool
 from tools.sec_tools import SEC10KTool, SEC10QTool
 
-from crewai_tools import WebsiteSearchTool, ScrapeWebsiteTool, TXTSearchTool
+from crewai_tools import WebsiteSearchTool, ScrapeWebsiteTool
 
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain.llms import Ollama
-llm = Ollama(model="llama3.1")
+llm = LLM(
+    model="deepseek/deepseek-chat",
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com",
+)
 
 @CrewBase
 class StockAnalysisCrew:
     agents_config = 'config/agents.yaml'
     tasks_config = 'config/tasks.yaml'
-    
-    @agent
-    def financial_agent(self) -> Agent:
-        return Agent(
-            config=self.agents_config['financial_analyst'],
-            verbose=True,
-            llm=llm,
-            tools=[
-                ScrapeWebsiteTool(),
-                WebsiteSearchTool(),
-                CalculatorTool(),
-                SEC10QTool("AMZN"),
-                SEC10KTool("AMZN"),
-            ]
-        )
-    
-    @task
-    def financial_analysis(self) -> Task: 
-        return Task(
-            config=self.tasks_config['financial_analysis'],
-            agent=self.financial_agent(),
-        )
-    
 
     @agent
     def research_analyst_agent(self) -> Agent:
@@ -72,8 +52,8 @@ class StockAnalysisCrew:
                 ScrapeWebsiteTool(),
                 WebsiteSearchTool(),
                 CalculatorTool(),
-                SEC10QTool(),
-                SEC10KTool(),
+                SEC10QTool("AMZN"),
+                SEC10KTool("AMZN"),
             ]
         )
     
